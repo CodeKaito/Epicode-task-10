c971c53392d8167fd660056649e01f74558e4ca3
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-11-24 18:20:27 +0100

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let commentSent = () => alert("Comment Sent Successfully!");
+
+let changeTitle = () => {
+    newTitle = prompt("Inserisci un nuovo titolo");
+    if (newTitle !== null) {
+        document.getElementById("title").textContent = newTitle;
+    }
+}
+
+let changeBackground = () => {
+    var newBackground = document.getElementsByTagName("body")[0];
+    newBackground.style.background = "#0166CC";
+}
+
+let changeAddress = (address) => {
+    var newAddress = address;
+    document.getElementById("address").textContent = newAddress;
+}
 
